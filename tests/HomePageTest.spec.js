4137144c9d86e47c6560b7190184fb226cf89a9c
Rilsteel-git <syahrill.ramadhan02@gmail.com>
2025-05-01 18:48:26 +0700

    Test case goto home page

---
+++ tests/HomePageTest.spec.js
@@ -0,0 +1,17 @@
+const { test, expect } = require("@playwright/test");
+
+test("Home page", async ({ page }) => {
+  await page.goto("https://demoblaze.com/index.html");
+
+  const pageTitle = page.title("STORE");
+  console.log("Page title is:", pageTitle);
+
+  await expect(page).toHaveTitle("STORE");
+
+  const pageURL = page.url();
+  console.log("Page url is:", pageURL);
+
+  await expect(page).toHaveURL("https://demoblaze.com/index.html");
+
+  await page.close();
+});
